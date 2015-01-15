64ceee8ff575d01ba4c8aed71a62394d4bb63fad
Marcelo Figueira <marcelofigueirapereira@gmail.com>
2015-01-15 10:46:41 -0300

    Correção do arquivo de require

--- lib/livro.rb
+++ lib/livro.rb
@@ -1,7 +1,9 @@
 # coding: utf-8
 class Livro
 
-	attr_accessor :preco
+	attr_accessor :preco 
+	#com o uso do método attr_accessor temos acesso de leitura e escrita sobre o atributo
+	#é como o metodo get e set do Java
 
 	def initialize(autor, isbn = "1", numero_de_paginas, preco)
 		@autor = autor
--- lib/loja_virtual.rb
+++ lib/loja_virtual.rb
@@ -1,2 +1,2 @@
 require File.expand_path('lib/livro')
-require File.expand_path('lib,biblioteca')
+require File.expand_path('lib/biblioteca')
