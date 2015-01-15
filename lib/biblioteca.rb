abf3b78a87a606beea4a0410aaf2a872e41899b5
Marcelo Figueira <marcelofigueirapereira@gmail.com>
2015-01-15 12:02:43 -0300

    mundando a forma de guardar os livros de array para hash

--- lib/biblioteca.rb
+++ lib/biblioteca.rb
@@ -3,10 +3,14 @@ class Biblioteca
 	attr_reader :livros
 
 	def initialize
-		@livros = []
+		#@livros = [] Assim inicializava com Array
+		@livros = {} #Assim inicializa com Hash
 	end
 
 	def adiciona(livro)
-		@livros << livro
+		#@livros << livro Assim acionavamos algo ao array
+
+		@livros[livro.categoria] || = []
+		@livros[livro.categoria] << livro
 	end
 end
