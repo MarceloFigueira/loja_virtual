17225a83aec103a0320d58c19578c41a4cf2e332
Marcelo Figueira <marcelofigueirapereira@gmail.com>
2015-01-15 11:21:58 -0300

    adicionado atributo categorias a class livro.rb

--- lib/livro.rb
+++ lib/livro.rb
@@ -4,15 +4,17 @@ class Livro
 	attr_accessor :valor
 	#com o uso do método attr_accessor temos acesso de leitura e escrita sobre o atributo
 	#é como o metodo get e set do Java
+	attr_reader :categoria
 
-	def initialize(autor, isbn = "1", numero_de_paginas, valor)
+	def initialize(autor, isbn = "1", numero_de_paginas, valor, categoria)
 		@autor = autor
 		@isbn = isbn
 		@numero_de_paginas = numero_de_paginas
 		@valor = valor
+		@categoria = categoria
 	end
 
 	def to_s
-		"Autor: #{@autor}, ISBN: #{@isbn}, Páginas: #{@numero_de_paginas}"
+		"Autor: #{@autor}, ISBN: #{@isbn}, Páginas: #{@numero_de_paginas}, Categoria: #{@categoria}"
 	end
 end
