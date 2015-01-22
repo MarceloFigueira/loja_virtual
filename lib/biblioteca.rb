ed7b1388ce780711300a13ec4ac9700e435d179e
Marcelo Figueira <marcelofigueirapereira@gmail.com>
2015-01-22 18:07:34 -0300

    update da class biblioteca

--- lib/biblioteca.rb
+++ lib/biblioteca.rb
@@ -5,13 +5,17 @@ class Biblioteca
 	def initialize
 		#@livros = [] Assim inicializava com Array
 		@livros = {} #Assim inicializa com Hash
+		@banco_de_arquivos = BancoDeArquivos.new
+		#A linha acima inicializa banco de arquivos
 	end
 
 	def adiciona(livro)
 		#@livros << livro Assim acionavamos algo ao array
-
-		@livros[livro.categoria] ||= []
-		@livros[livro.categoria] << livro
+		salva livro do
+			@livros[livro.categoria] ||= []
+			@livros[livro.categoria] << livro
+		end
+		
 	end
 
 	def livros
@@ -22,5 +26,12 @@ class Biblioteca
 		@livros[categoria].each do |livro|
 			yield livro if block_given?
 		end
+	end
+
+	private
+
+	def salva(livro)
+		@banco_de_arquivos.salva livro
+		yield
 	end
 end
