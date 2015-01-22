7a1c9e07437975f5a0ec387cc93c5609ecc22c8d
Marcelo Figueira <marcelofigueirapereira@gmail.com>
2015-01-22 16:55:00 -0300

    update de tudo

--- lib/biblioteca.rb
+++ lib/biblioteca.rb
@@ -10,7 +10,17 @@ class Biblioteca
 	def adiciona(livro)
 		#@livros << livro Assim acionavamos algo ao array
 
-		@livros[livro.categoria] || = []
+		@livros[livro.categoria] ||= []
 		@livros[livro.categoria] << livro
+	end
+
+	def livros
+		@livros.values.flatten
+	end
+
+	def livros_por_categoria(categoria)
+		@livros[categoria].each do |livro|
+			yield livro if block_given?
+		end
 	end
 end
--- lib/livro.rb
+++ lib/livro.rb
@@ -4,17 +4,26 @@ class Livro
 	attr_accessor :valor
 	#com o uso do método attr_accessor temos acesso de leitura e escrita sobre o atributo
 	#é como o metodo get e set do Java
-	attr_reader :categoria
+	attr_reader :categoria, :autor, :titulo
 
-	def initialize(autor, isbn = "1", numero_de_paginas, valor, categoria)
+	def initialize(titulo, autor, isbn = "1", numero_de_paginas, valor, categoria)
+		@titulo = titulo
 		@autor = autor
 		@isbn = isbn
 		@numero_de_paginas = numero_de_paginas
-		@valor = valor
 		@categoria = categoria
+		@valor = valor
 	end
 
 	def to_s
 		"Autor: #{@autor}, ISBN: #{@isbn}, Páginas: #{@numero_de_paginas}, Categoria: #{@categoria}"
+	end
+
+	def eql?(outro_livro)
+		@isbn == outro_livro.isbn
+	end
+
+	def hash
+		@isbn.hash
 	end
 end
